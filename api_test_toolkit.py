b3fd93a1007e88f66fbb779e727931a9984ee92d
Hussam Ammaya <Hussam.ammaya@gmail.com>
2026-07-20 12:15:28 +0200

    Add get_env_var with fail-fast validazion

--- api_test_toolkit.py
+++ api_test_toolkit.py
@@ -1,20 +1,35 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 
-def fetch_data(url):
-        response = requests.get(url)
+def get_env_var(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise RuntimeError(
+              f"Environment variable '{name}' is not set."
+            f"Check that your .env file exists and contains it."
+        )
+    return value
+
+BASE_URL = get_env_var("BASE_")
+
+def fetch_data(BASE_URL):
+        response = requests.get(BASE_URL)
         response.raise_for_status()
         return response.json()
     
 
-def fetch_status(url):
-    response = requests.get(url)
+def fetch_status(BASE_URL):
+    response = requests.get(BASE_URL)
     return response.status_code
 
 
-def send_data(url, payload):
-    response = requests.post(url, json=payload)
+def send_data(BASE_URL, payload):
+    response = requests.post(BASE_URL, json=payload)
     response.raise_for_status()
     return response.json()
 
@@ -23,6 +38,3 @@ def get_required_fields(data, required_fields):
         if field not in data:
             raise KeyError(f"Missing required field: {field}")
     return {field: data[field] for field in required_fields}
-
-data2 = {"name": "Ahmad", "email": "ahmad@example.com"}
-print(get_required_fields(data2, ["name", "email"]))      
